248eaec7aa974e4f3019aefcae716bdbfff36654
gzapodea <gzapodea@cisco.com>
2019-06-06 19:42:32 -0700

    initial commit

---
+++ config.py
@@ -0,0 +1,45 @@
+
+
+# developed by Gabi Zapodeanu, TME, ENBU, Cisco Systems
+
+
+# This file contains the:
+# Webex Teams Auth and URL
+# DNAC username and password, server info
+# ServiceNow developer instance info, username and password
+# PubNub Keys
+
+# Update this section with your PubNub keys
+PUB_KEY = 'pub-c-b1336069-...'
+SUB_KEY = 'sub-c-26cd15ca-...'
+SEC_KEY = 'sec-c-NWY2NmY3OT...'
+CHANNEL = '...'
+
+
+# Update this section with the Webeex Teams token for each student
+WEBEX_TEAMS_URL = 'https://api.ciscospark.com/v1'
+WEBEX_TEAMS_AUTH = 'Bearer ' + ''
+WEBEX_TEAMS_SPACE = 'Programmability Blitz Event Space'  # to be used when new instance created in ServiceNow
+
+
+# Update this section with the DNA Center server info and user information
+DNAC_URL = 'https://1.1.1.1'
+DNAC_USER = 'admin'
+DNAC_PASS = 'password'
+
+# Update this section with the Service Now instance to be used for labs
+SNOW_URL = 'https://dev12345.service-now.com/api/now'
+SNOW_ADMIN = 'admin'
+SNOW_DEV = 'dev'
+SNOW_PASS = 'password'
+SNOW_INSTANCE = 'dev12345'
+
+
+# Update this section with the info for the Catalyst 9k to be used during the IOS XE Module
+IOS_XE_HOST = '10.1.1.1'
+IOS_XE_USER = 'cisco'
+IOS_XE_PASS = 'cisco'
+
+UNAUTH_USER = 'blitz'
+
+FOLDER_NAME = '/bootflash/DEVNET3841'
